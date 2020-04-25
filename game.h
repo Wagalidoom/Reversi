d31f3ab5bade26af937378d4d8b84dd661254095
Lukas Balandraux <lukas-8@live.fr>
2020-04-25 14:56:58 +0200

    define first struct

---
+++ game.h
@@ -0,0 +1,20 @@
+// DEFINE
+
+#define LIGNE 8
+#define COLONNE 8
+
+// STRUCTURE
+
+typedef struct
+{
+    int alive;
+    int color;
+    int coordX;
+    int coordY;
+} pion;
+
+typedef struct
+{
+    pion plateau[LIGNE][COLONNE];
+} game;
+
