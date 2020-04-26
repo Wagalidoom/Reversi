47df4a10af1222907770402bfaa6d63eb98cd261
JordanCosio <jorcos@hotmail.fr>
2020-04-26 19:40:56 +0200

    case_existe

--- game.c
+++ game.c
@@ -1,5 +1,6 @@
 #include "game.h"
-//#include <stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 game init_plateau()
 {
@@ -45,9 +46,7 @@ int coup_valide(game plateau, coup coup_)
     char actual_case = plateau.plateau_tab[coordX][coordY].pion;
     direction dir[8] = {HAUT, BAS, GAUCHE, DROITE, DIAG_HD, DIAG_BD, DIAG_HG, DIAG_BG};
 
-    for (direction dir_ = HAUT; dir <= DIAG_BG; dir_++){
-
-    }
+    
 
 
     if ((plateau.plateau_tab[coordX - 1][coordY].pion != actual_case) && (plateau.plateau_tab[coordX - 1][coordY].pion != '.'))
@@ -69,4 +68,55 @@ int coup_valide(game plateau, coup coup_)
     {
         //check_direction(plateau, coup, BAS);
     }
+}
+
+int case_existe(case_ case_actuelle, direction dir){
+    int deplacement_x, deplacement_y, new_x, new_y;
+    switch (dir)
+    {
+    case HAUT:
+        deplacement_x = 0;
+        deplacement_y = -1;
+        break;
+    case BAS:
+        deplacement_x = 0;
+        deplacement_y = 1;
+        break;
+    case GAUCHE:
+        deplacement_x = -1;
+        deplacement_y = 0;
+        break;
+    case DROITE:
+        deplacement_x = 1;
+        deplacement_y = 0;
+        break;
+    case DIAG_HD:
+        deplacement_x = 1;
+        deplacement_y = -1;
+        break;
+    case DIAG_BD:
+        deplacement_x = 1;
+        deplacement_y = 1;
+        break;
+    case DIAG_HG:
+        deplacement_x = -1;
+        deplacement_y = -1;
+        break;
+    case DIAG_BG:
+        deplacement_x = -1;
+        deplacement_y = 1;
+        break;
+    
+    
+    default:
+        printf("Erreur sur la vérifiction du déplacement");
+        break;
+    }
+
+    new_x = case_actuelle.coordX + deplacement_x;
+    new_y = case_actuelle.coordY + deplacement_y;
+    if ( (new_x < 0) || (new_x > LIGNE) || (new_y < 0) || (new_y > COLONNE) ){
+        return false;
+    }
+    return true;
 }
--- main.c
+++ main.c
@@ -4,5 +4,13 @@ int main()
 {
 	game jeu = init_plateau();
 	afficher_plateau(jeu);
+	case_ test_case;
+	test_case.coordX = 0;
+	test_case.coordY = 0;
+	direction dir_gauche = GAUCHE;
+	direction dir_bas = BAS;
+	direction dir_hd = DIAG_HD;
+	direction dir_droite = DROITE;
+	printf("Gauche : %d, bas : %d, haut_droite : %d, droite : %d", case_existe(test_case, dir_gauche), case_existe(test_case, dir_bas), case_existe(test_case, dir_hd), case_existe(test_case, dir_droite));
 	return 0;
 }
